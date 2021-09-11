8db01c7605c8fc3304246f45906f18ace7daa142
Alexandre <79406753+Wagowe@users.noreply.github.com>
2021-09-11 18:52:06 -0300

    API de portfolios
    - Carrega os modelos;
    - Faz a previsão dos preços futuros (d+10);
    - Gera portfolios aleatórios, com pesos e quantidade de ativos pré-definidos, e suas métricas de risco e retorno;
    - Seleciona portfolios mais adequado segundo os perfis de risco.

---
+++ Deploy/API/main.py
@@ -0,0 +1,71 @@
+import pandas as pd
+from flask import Flask, jsonify
+import requests
+from flask_restful import Resource, Api
+from pandas.io.parsers import read_csv
+import json
+#from gerador_portfolios import gera_portfolios
+
+
+app = Flask(__name__)
+api = Api(app)
+
+
+@app.route('/', methods = ['GET'])
+def get_portfolios():
+        
+    df_portfolios_escolhidos = pd.read_csv("dataframes/df_portfolios_escolhidos.csv", index_col=0)
+    
+    df_portfolios_escolhidos_json = df_portfolios_escolhidos.to_json(orient = "index")
+    
+    df_portfolios_escolhidos_json = json.loads(df_portfolios_escolhidos_json)
+    
+    df_portfolios_escolhidos_json = json.dumps(df_portfolios_escolhidos_json, indent=0) 
+    
+    return df_portfolios_escolhidos_json
+
+
+@app.route('/teste', methods = ['GET', 'POST'])
+def teste_portfolios():
+
+    from gerador_portfolios import gera_portfolios
+
+    horizonte = 10
+    lista_pesos = [0.05, 0.1 ,0.15, 0.2, 0.25, 0.3, 0.35, 0.4]
+    n_componentes = 5 # qtd. de ativos no portfolio.
+    n_iter = 1 # calibra a quantidade de arranjos aleatórios que cada combinação que soma 100% de pesos vai dar origem (valor ideal: 100)
+    risk_free = 0.03
+    target = 0.15
+
+    ativos = ['BTC-USD',
+    'DOGE-USD',
+    'XMR-USD',
+    'LTC-USD',
+    'DASH-USD',
+    'XLM-USD',
+    'XRP-USD',
+    'USDT-USD',
+    'ETH-USD',
+    'ETC-USD',
+    'NEO-USD',
+    'MKR-USD',
+    'MIOTA-USD',
+    'EOS-USD',
+    'BCH-USD',
+    'BNB-USD',
+    'TRX-USD',
+    'LINK-USD',
+    'ADA-USD',
+    'XTZ-USD',
+    'FIL-USD',
+    'THETA-USD']
+
+
+    df_retornos = pd.read_csv("dataframes/df_retornos.csv")
+
+    gera_portfolios(lista_ativos= ativos, df_retornos = df_retornos, lista_pesos=lista_pesos, n_componentes=n_componentes, n_iter=n_iter)
+    return "Criptofolio atualizado!"
+
+
+if __name__ == '__main__':
+    app.run()
