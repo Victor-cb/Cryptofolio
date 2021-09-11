54bee50ddf5c8494c7a452b078c134d7c296ae66
Alexandre <79406753+Wagowe@users.noreply.github.com>
2021-09-11 20:13:56 -0300

    Update gerador_portfolios.py

--- Deploy/API/gerador_portfolios.py
+++ Deploy/API/gerador_portfolios.py
@@ -6,7 +6,7 @@ import random
 
 from joblib import load
 
-from itertools import combinations_with_replacement, product
+from itertools import product
 
 horizonte = 10
 lista_pesos = [0.05, 0.1 ,0.15, 0.2, 0.25, 0.3, 0.35, 0.4]
