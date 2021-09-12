f407f3fe14b1f0eba41e6114735798b4ac81f08b
Victor-cb <brito.victorc@gmail.com>
2021-09-11 22:30:39 -0300

    ajuste API

--- routes.py
+++ routes.py
@@ -2,6 +2,9 @@ from flask import render_template, request,Flask
 import numpy as np
 import re
 import requests
+import pandas as pd
+import json
+from pandas.io.parsers import read_csv
 
 
 app = Flask(__name__)
@@ -47,7 +50,7 @@ def quest_perfil_page():
     else:
        perfil_final = "Apetite altissímo ao risco AKA, CHUCK NORRIS: YOLO, Você é uma pessoa que aceita e entende que 'com grandes poderes, vem grandes perdas', Uncle Ben. Mas vamos combinar, caso fique milionário, me chama pro churras. Mas se perder tudo amanhã, não chama a mamãe."
        indice_perfil ="Agressivo"
-    portfolios = requests.get("")
+    portfolios = requests.get("https://criptofolio.herokuapp.com/get_portfolios")
     portfolio_dict=portfolios.json()
     lista_peso_raw =portfolio_dict[indice_perfil]["pesos"]
     lista_peso = re.findall('[\-\+]?[0-9]*(\.[0-9]+)',lista_peso_raw)
@@ -61,7 +64,60 @@ def quest_perfil_page():
         dict_portfolio[lista_crypto[i]] = lista_peso[i]
         dict_portfolio_legenda[lista_crypto[i]] = lista_peso[i]
     return render_template('perfil.html',indice_perfil=indice_perfil,dict_portfolio = dict_portfolio, perfil_final=perfil_final,dict_portfolio_legenda=dict_portfolio_legenda )
+@app.route('/get_portfolios', methods = ['GET'])
+def get_portfolios():
+        
+    df_portfolios_escolhidos = pd.read_csv("dataframes/df_portfolios_escolhidos.csv", index_col=0)
+    
+    df_portfolios_escolhidos_json = df_portfolios_escolhidos.to_json(orient = "index")
+    
+    df_portfolios_escolhidos_json = json.loads(df_portfolios_escolhidos_json)
+    
+    df_portfolios_escolhidos_json = json.dumps(df_portfolios_escolhidos_json, indent=0) 
+    
+    return df_portfolios_escolhidos_json
 
+
+@app.route('/update_portfolios', methods = ['GET', 'POST'])
+def update_portfolios():
+
+    from gerador_portfolios import gera_portfolios
+
+    horizonte = 10
+    lista_pesos = [0.05, 0.1 ,0.15, 0.2, 0.25, 0.3, 0.35, 0.4]
+    n_componentes = 5 # qtd. de ativos no portfolio.
+    n_iter = 1 # calibra a quantidade de arranjos aleatórios que cada combinação que soma 100% de pesos vai dar origem (valor ideal: 100)
+    risk_free = 0.03
+    target = 0.15
+
+    ativos = ['BTC-USD',
+    'DOGE-USD',
+    'XMR-USD',
+    'LTC-USD',
+    'DASH-USD',
+    'XLM-USD',
+    'XRP-USD',
+    'USDT-USD',
+    'ETH-USD',
+    'ETC-USD',
+    'NEO-USD',
+    'MKR-USD',
+    'MIOTA-USD',
+    'EOS-USD',
+    'BCH-USD',
+    'BNB-USD',
+    'TRX-USD',
+    'LINK-USD',
+    'ADA-USD',
+    'XTZ-USD',
+    'FIL-USD',
+    'THETA-USD']
+
+
+    df_retornos = pd.read_csv("dataframes/df_retornos.csv")
+
+    gera_portfolios(lista_ativos= ativos, df_retornos = df_retornos, lista_pesos=lista_pesos, n_componentes=n_componentes, n_iter=n_iter)
+    return "Criptofolio atualizado!"
 
 if __name__ == '__main__':
     app.run(debug=True)
