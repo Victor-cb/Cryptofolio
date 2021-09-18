105cf809d0ef60abfe69abc5c8739a534025bf76
Victor-cb <brito.victorc@gmail.com>
2021-09-18 13:13:51 -0300

    Add Link + Descrição

--- routes.py
+++ routes.py
@@ -6,33 +6,45 @@ import pandas as pd
 import json
 from pandas.io.parsers import read_csv
 
-
+#Iniciando a aplicação Flask
 app = Flask(__name__)
+#Endpoint Main - Direto no questionário do perfil
 @app.route('/')
 def perfil_questions():
     return render_template('quest_perfil.html')
+
+#Perfil Home, onde vamos explicar e apresentar o projeto.
 @app.route('/home')
 def home_page():
     return render_template('home.html')
+
+#Página sobre a equipe que realizou o projeto
 @app.route('/about_us')
 def about_us_page():
     return render_template('about_us.html')
-@app.route('/perfil')
+
+#Primeira tentativa de resgatar dados do questionario - Falha. 
+"""@app.route('/perfil')
 def perfil_page():
     if request.method == 'POST':
         print(request.form.getlist('radio'))
         return 'Done'
-    return render_template('perfil.html')
+    return render_template('perfil.html')"""
+
+#Aqui será a rota onde após pegar todos os dados do questionário, vamos criar a página de portfolio.
 @app.route('/quest_perfil',methods = ['POST'])
 def quest_perfil_page():
     #pesos = [3,2,2,4,2,3,2,3,2,2,2,2]
+    #Peso atribuido a cada umas das 7 perguntas do questionário.
     pesos = [3,4,6,3,5,4,4]
+    #a página retorna um dict com key = p* sendo * o número da pergunta, e value o valor númerio da resposta
     resposta_forms= list()
     for key, val in request.form.items():
         resposta_forms.append(val)
+    #transformando lista de respostas em valores númericos.
     for i in range(0, len(resposta_forms)):
         resposta_forms[i] = int(resposta_forms[i])
-
+    #aplicando peso de cada pergunta, e inserindo o perfil
     produto = np.multiply(pesos,resposta_forms)
     nota_perfil = sum(produto)
     if nota_perfil <= 52:
@@ -50,20 +62,41 @@ def quest_perfil_page():
     else:
        perfil_final = "Apetite altissímo ao risco AKA, CHUCK NORRIS: YOLO, Você é uma pessoa que aceita e entende que 'com grandes poderes, vem grandes perdas', Uncle Ben. Mas vamos combinar, caso fique milionário, me chama pro churras. Mas se perder tudo amanhã, não chama a mamãe."
        indice_perfil ="Agressivo"
+    #buscando os 5 portfolio do JSON criado no gerador_portfolios.py
     portfolios = requests.get("https://criptofolio.herokuapp.com/get_portfolios")
     portfolio_dict=portfolios.json()
+    # Selecionando o portfolio que foi adequado ao questionário
     lista_peso_raw =portfolio_dict[indice_perfil]["pesos"]
+    #regex visto que a saira era uma string "[]"
     lista_peso = re.findall('[\-\+]?[0-9]*(\.[0-9]+)',lista_peso_raw)
     lista_peso =list(map(float,lista_peso))
     lista_crypto_raw = portfolio_dict[indice_perfil]["cryptos"]
     lista_crypto =  re.findall('[A-Z]+-[A-Z]+',lista_crypto_raw)
     dict_portfolio ={}
     dict_portfolio_legenda={}
+    #Add Description from csv and Link
+    df = pd.read_csv('dados/df_eligible.csv')
+    df['link']=df['description'].str.extract(r'(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9]\
+    [a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})')
+    dict_description = dict(zip(df.ticker,df.description))
+    dict_link =  dict(zip(df.ticker,df.link))
+    dic_final_desc = {}
+    dic_link = {}
+    for i in range(0,len(lista_crypto)):
+        if lista_crypto[i]  in dict_description:
+            dic_final_desc[lista_crypto[i]] = dict_description[lista_crypto[i]]
+        if lista_crypto[i] in dict_link:
+            dic_link[lista_crypto[i]] = dict_link[lista_crypto[i]]
+    #Dicionário novo criado somente para uso da API google de gráficos.
     dict_portfolio_legenda['Ativo']='Peso' 
     for i in range(0,len(lista_peso)):
         dict_portfolio[lista_crypto[i]] = lista_peso[i]
         dict_portfolio_legenda[lista_crypto[i]] = lista_peso[i]
-    return render_template('perfil.html',indice_perfil=indice_perfil,dict_portfolio = dict_portfolio, perfil_final=perfil_final,dict_portfolio_legenda=dict_portfolio_legenda )
+    
+    
+    return render_template('perfil.html',indice_perfil=indice_perfil,dict_portfolio = dict_portfolio, perfil_final=perfil_final,
+                            dict_portfolio_legenda=dict_portfolio_legenda, dic_final_desc=dic_final_desc,dic_link=dic_link)
+
 @app.route('/get_portfolios', methods = ['GET'])
 def get_portfolios():
         
